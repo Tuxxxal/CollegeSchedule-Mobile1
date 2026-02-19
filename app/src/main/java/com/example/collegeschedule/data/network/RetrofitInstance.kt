6df4c3c7d2cf1743439d954903b0e29d9647bade
Vadim <el.soback2001@.mail.ru>
2026-02-19 12:08:43 +0700

    update RetrofitInstance

--- app/src/main/java/com/example/collegeschedule/data/network/RetrofitInstance.kt
+++ app/src/main/java/com/example/collegeschedule/data/network/RetrofitInstance.kt
@@ -1,19 +1,14 @@
 package com.example.collegeschedule.data.network
 
-import com.example.collegeschedule.data.dto.ScheduleByDateDto
-import retrofit2.http.GET
-import retrofit2.http.Path
-import retrofit2.http.Query
+import com.example.collegeschedule.data.api.ScheduleApi
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
-interface ScheduleApi {
+object RetrofitInstance {
+    private val retrofit = Retrofit.Builder()
+        .baseUrl("http://10.0.2.2:5215/")
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
 
-    @GET("api/schedule/group/{groupName}")
-    suspend fun getSchedule(
-        @Path("groupName") groupName: String,
-        @Query("start") start: String,
-        @Query("end") end: String
-    ): List<ScheduleByDateDto>
-
-    @GET("api/schedule/groups")
-    suspend fun getGroups(): List<String>
+    val api: ScheduleApi = retrofit.create(ScheduleApi::class.java)
 }
